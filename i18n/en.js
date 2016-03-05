670ccbd6c0218e336a19d0dd912189c968cd0ef2
Игорь Щербаков <i.v.shcherbakov@gmail.com>
2016-03-05 23:45:24 +0600

    i18n

--- i18n/en.js
+++ i18n/en.js
@@ -1,10 +1,4 @@
 {
-	"slogan": "One tap to get syncs done",
-	"Download": "Download",
-	"apk": "Android app",
-	"current": "current",
-	"language": "язык",
-	"What's new": "What's new",
 	"Downloads": "Downloads",
 	"GitHub": "GitHub",
 	"vk": "vk",
--- i18n/ru.js
+++ i18n/ru.js
@@ -1,14 +1,9 @@
 {
-	"slogan": "Твои аудиозаписи всегда с тобой",
-	"Download": "Скачать",
-	"apk": "приложение для Android",
-	"current": "текущий",
-	"language": "language",
 	"Downloads": "Загрузки",
-	"What's new": "Что нового",
 	"GitHub": "GitHub",
 	"vk": "vk",
 	"en": "en",
 	"ru": "ru",
-	"Preview": "Preview"
+	"Preview": "Анонс",
+	"slogan": "slogan"
 }
